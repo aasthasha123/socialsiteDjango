9694edcb4125c74dea2c1e5ec9119534ff0ca7c7
Aastha Sharma <aasthasharma738@gmail.com>
2021-01-05 12:56:56 +0530

    added accounts, posts and groups app.

---
+++ accounts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import User, PermissionsMixin
+
+class User(User, PermissionsMixin):
+    def __str__(self):
+        return "@{}".format(self.username)
---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from . import forms
+
+class SignUpView(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login') # on succesful signup
+    template_name = 'accounts/signup.html'
+    
